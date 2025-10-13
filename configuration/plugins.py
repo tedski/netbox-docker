e4c59da3e93b48509026c778d53e321624c2c255
Ted Strzalkowski <tedski@gmail.com>
2025-10-13 09:19:34 -0700

    Create custom build for plugins

--- configuration/plugins.py
+++ configuration/plugins.py
@@ -1,13 +1,12 @@
-# Add your plugins and plugin settings here.
-# Of course uncomment this file out.
-
 # To learn how to build images with your required plugins
 # See https://github.com/netbox-community/netbox-docker/wiki/Using-Netbox-Plugins
 
-# PLUGINS = ["netbox_bgp"]
+PLUGINS = ["netbox_topology_views"]
 
-# PLUGINS_CONFIG = {
-#   "netbox_bgp": {
-#     ADD YOUR SETTINGS HERE
-#   }
-# }
+PLUGINS_CONFIG = {
+    'netbox_topology_views': {
+        'static_image_directory': 'netbox_topology_views/img',
+        'allow_coordinates_saving': True,
+        'always_save_coordinates': True
+    }
+}
